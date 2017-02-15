d23310b577fb7c5a0a340799070482d03142ba20
Siva Muthusamy <s.b.muthusamy@gmail.com>
2017-02-15 03:53:11 -0500

    Added aws region to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.paperclip_defaults = {
     :storage => :s3,
+    s3_region: 'us-west-2',
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
