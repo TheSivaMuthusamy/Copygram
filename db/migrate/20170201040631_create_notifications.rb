4c65cbcfa67316596403315be2daa61b92382638
Siva Muthusamy <s.b.muthusamy@gmail.com>
2017-02-15 03:39:45 -0500

    Fixed Databases

--- db/migrate/20170201040631_create_notifications.rb
+++ db/migrate/20170201040631_create_notifications.rb
@@ -2,7 +2,7 @@ class CreateNotifications < ActiveRecord::Migration[5.0]
   def change
     create_table :notifications do |t|
       t.references :user, foreign_key: true, index: true
-      t.references :subscribed_user, foreign_key: true,  index: true
+      t.references :subscribed_user, index: true
       t.references :post, foreign_key: true, index: true
       t.integer :identifier
       t.string :notice_type
